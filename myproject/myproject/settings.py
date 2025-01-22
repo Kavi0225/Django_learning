e047b30124f0701638688b399fe87af9fa3725f9
unknown <you@example.com>
2025-01-22 16:52:05 +0530

    updated till day17 django

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travel.apps.TravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoDB',
+        'USER' :'postgres',
+        'PASSWORD' : '6233',
+        'HOST' : 'localhost'
     }
 }
 
--- myproject/travel/views.py
+++ myproject/travel/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.desc = "The City That Never Sleep"
+    dest1.img = "destination_4.jpg"
+    dest1.price = 750
+    dest1.offer = False
+
+    dest2 = Destination()
+    dest2.name = "Dehli"
+    dest2.desc = "The City Capital Of India"
+    dest2.img = "destination_3.jpg"
+    dest2.price = 800
+    dest2.offer = False
+
+    dest3 = Destination()
+    dest3.name = "Pune"
+    dest3.desc = "High Tech City"
+    dest3.img = "destination_9.jpg"
+    dest3.price = 650
+    dest3.offer = True
+
+    dest = [dest1, dest2 , dest3]
+
+    return render(request, "index.html", {'dest' : dest})
 def about(request):
     return render(request, "about.html")
+
